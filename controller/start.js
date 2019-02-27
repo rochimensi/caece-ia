52ef2fad1701560a720445c1eb38e80190401ddb
LAPTOP-0HN0E0VI\NG <ng@qbkconsulting.com>
2019-02-27 02:08:03 -0300

    ajuste clasificador

--- controller/start.js
+++ controller/start.js
@@ -6,17 +6,11 @@ app.listen(3000, function() {
     console.log('server running on port 3000'); 
 } ) 
   
-app.get('/secondClassification', secondClassification); 
+app.get('/classify', classify); 
 
-app.get('/firstClassification', firstClassification); 
+function classify(req, res) { 
 
-app.get('/nameClassifier', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-  
-function secondClassification(req, res) { 
-
-    var myPythonScriptPath = 'tflow/scripts/label_image.py';
+    var myPythonScriptPath = 'tflow/scripts/classify.py';
 
     // Use python shell
     const {PythonShell} = require("python-shell");
@@ -30,29 +24,5 @@ function secondClassification(req, res) {
         if (err){
             throw err;
         };
-
-        console.log('finished');
-    });
-} 
-
-
-function firstClassification(req, res) { 
-
-    var myPythonScriptPath = 'tflow/scripts/first-classification.py';
-
-    // Use python shell
-    const {PythonShell} = require("python-shell");
-    var pyshell = new PythonShell(myPythonScriptPath);
-
-    pyshell.on('message', function (message) {
-        console.log(message);
-    });
-
-    pyshell.end(function (err) {
-        if (err){
-            throw err;
-        };
-
-        console.log('finished');
     });
 }
